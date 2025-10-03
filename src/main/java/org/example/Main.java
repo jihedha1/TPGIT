179c92edbc60364c18f3a5d89f338c4ecccaff2d
jihedha1 <jihedhallem1234@gmail.com>
2025-10-03 13:07:58 +0100

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome! I'm dev ");
+        System.out.print("Hello and welcome! I'm dev branch  ");
 
     }
 }
